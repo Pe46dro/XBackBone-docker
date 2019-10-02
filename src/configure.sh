2503785c440cda3d1a1ddca43fbc18e6ab923cb8
Amir Zarrinkafsh <nightah@me.com>
2019-10-03 00:26:39 +1000

    Add ability to specify application name with an env variable

--- src/configure.sh
+++ src/configure.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 sed -i "s@http:\/\/localhost@$URL@g" /app/config.php
+sed -i "3i\ \ \ \ \ \ \ \ \'app_name\' => \'$APP_NAME\'," /app/config.php
 
 if [ ! -f /app/resources/database/xbackbone.db ]; then
 	cd /app
