2bb5cfcdfa0f77993222df776ef4be2b653506cd
Pietro Marangon <pietro.marangon@gmail.com>
2019-12-14 21:38:40 +0100

    [ci skip]

--- src/configure.sh
+++ src/configure.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+export PHP_POST_MAX_SIZE=$PHP_UPLOAD_MAX_FILESIZE
+
+sed -i "s@50m@${PHP_UPLOAD_MAX_FILESIZE:-50m}; }@g" 10-general.conf
 sed -i "s@http:\/\/localhost@$URL@g" /app/config.php
 sed -i "s/return\ \[/&\n\ \ \ \ \'app_name\' => \'$APP_NAME\',/" /app/config.php
 
