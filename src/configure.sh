8261f3ad2ae77044295e63e3b027eaa39f58ba04
Pietro Marangon <pietro.marangon@gmail.com>
2019-12-14 21:50:44 +0100

    Update configure.sh

--- src/configure.sh
+++ src/configure.sh
@@ -2,7 +2,7 @@
 
 export PHP_POST_MAX_SIZE=$PHP_UPLOAD_MAX_FILESIZE
 
-sed -i "s@50m@${PHP_UPLOAD_MAX_FILESIZE:-50m}; }@g" 10-general.conf
+sed -i "s@50m@${PHP_UPLOAD_MAX_FILESIZE:-50m}@g" /opt/docker/etc/nginx/vhost.common.d/10-general.conf
 sed -i "s@https:\/\/localhost@$URL@g" /app/config.php
 sed -i "s/return\ \[/&\n\ \ \ \ \'app_name\' => \'$APP_NAME\',/" /app/config.php
 
